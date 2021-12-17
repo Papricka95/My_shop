b7927432fd42cd6e8c9dd4cd564ce05862d78e94
Papricka95 <wadim.karlov@mail.ru>
2021-12-17 17:15:45 +0400

    create: book model and register her

--- my_shop/books_shop/models.py
+++ my_shop/books_shop/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
+
 # Create your models here.
+class Book(models.Model):
+    CHOICE_PUBLISHER = (
+        ('ast', 'АСТ'),
+        ('eksmo', 'Эксмо'),
+    )
+    #
+    # POSITION_CHOICES = (
+    #     ('senior', 'Senior'),
+    #     ('middle', 'Middle'),
+    #     ('junior', 'Junior')
+    # )
+
+    # PYTHON = 'python'
+    # JAVASCRIPT = 'javascript'
+    # CS = 'c#'
+    # CPP = 'cpp'
+    #
+    # LANGUAGE_CHOICES = (
+    #     (PYTHON, 'Python'),
+    #     (JAVASCRIPT, 'Javascript'),
+    #     (CS, 'C#'),
+    #     (CPP, 'C++')
+    # )
+    name_book = models.CharField(max_length=255, verbose_name='Название книги')
+    name_author = models.CharField(max_length=50, verbose_name='Имя автора')
+    surname_author = models.CharField(max_length=50, verbose_name='Фамилия автора')
+    date_of_publication = models.DateField(auto_now_add=False, auto_now=False)
+    publisher = models.CharField(max_length=20, choices=CHOICE_PUBLISHER)
+    salary = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def __str__(self):
+        return 'Название книги - {0}, Имя Автора - {1}, Фамилия автора - {2}, Дата публикации - {3}, Издатель - {4}, Цена - {5}'.format(
+            self.name_book, self.name_author, self.surname_author, self.date_of_publication, self.publisher,
+            self.salary)
+
+    def dict(self):
+        obj = {
+            'name': self.name_book,  # ссылка на поле класса Human
+            'surname': self.name_author,
+            'birth': self.surname_author,
+            'company': self.date_of_publication,
+            'position': self.publisher,
+            # 'language': self.language,
+            'salary': self.salary
+        }
+        return obj
--- my_shop/books_shop/views.py
+++ my_shop/books_shop/views.py
@@ -2,15 +2,27 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
 
 
-# class MainView(TemplateView):
-#     template_name = 'main_page.html'
-#     books_form = BookForm
-#
-#     def get(self, request):
-#         ctx = {}
-#
-def main_page(request):
-    return HttpResponse('Приветствую вас в моем магазине! Скоро он откроется!')
+class MainView(TemplateView):
+    template_name = 'main_page.html'
+    ctx = {}
+
+    def get(self, request):
+
+        return render(request, self.template_name, ctx)
+
+
+class RegisterFormView(FormView):
+    template_name = 'register.html'  # что отобразить пользователю с нашей формой
+    # form_class = UserCreateForm
+    success_url = '/books_shop/login'  # этот урл будет срабатывать в случае успешной регистрации
+
+    def form_valid(self, form):
+        form.save()
+        return super(RegisterFormView, self).form_valid(form)
+
+# save views as function for some time (their need do as class after some time)
+# def main_page(request):
+#     return HttpResponse('Приветствую вас в моем магазине! Скоро он откроется!')
