232f41b8712ffe962d22e79d1cb15d038b02f3f7
Papricka95 <wadim.karlov@mail.ru>
2021-12-24 01:20:53 +0400

    add: in views add LoginFormView(FormView) with validation

--- my_shop/books_shop/views.py
+++ my_shop/books_shop/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -11,7 +13,9 @@ class MainView(TemplateView):
 
     def get(self, request):
         ctx = {}
+
         return render(request, self.template_name, ctx)
+        # return JsonResponse({'Response': true})
 
 
 class RegisterFormView(FormView):
@@ -22,6 +26,24 @@ class RegisterFormView(FormView):
     def form_valid(self, form):
         form.save()
         return super(RegisterFormView, self).form_valid(form)
+
+
+class LoginFormView(FormView):
+    form_class = AuthenticationForm
+    template_name = 'login.html'
+    success_url = '/books-shop/'
+
+    # валидация формы
+    def form_valid(self, form):
+        # получаем пользователя на основе введенных в форму данных
+        self.user = form.get_user()
+        # логиним user
+        login(self.request, self.user)
+        # возвращаем метод предка LoginFormView form_valid
+        return super(LoginFormView, self).form_valid(form)
+
+    def form_invalid(self, form):
+        return super(LoginFormView, self).form_invalid(form)
 
 # save views as function for some time (their need do as class after some time)
 # def main_page(request):
